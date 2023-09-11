0309fad7d4101207bde69fd28d7b82021c0f5942
maximalekseenko <max.alekseenko@gmail.com>
2023-09-11 10:24:14 +0300

    button fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-jQuery(function() 
+gui = require('nw.gui');
+gui.Window.get().showDevTools();
+
+$(function() 
 {
     $( ".button-gen-new" ).on( "click", function() {
-        $( ".text-pass-new" ).html = Gen();
+        $( ".text-pass-new" ).html(GenNew());
     })
-})
+});
